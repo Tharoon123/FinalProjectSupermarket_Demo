f74e5570521668d7da7e141f484dc31584afcd39
Tharoon123 <64660264+Tharoon123@users.noreply.github.com>
2024-11-14 21:27:55 +0530

    origin

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import HomePage from './Components/HomePage';
+
+function App() {
+
+    
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzczp6cdxfyr1OndUx9ZGssI-NU9hMZa0",
+  authDomain: "finalproject11142024.firebaseapp.com",
+  projectId: "finalproject11142024",
+  storageBucket: "finalproject11142024.firebasestorage.app",
+  messagingSenderId: "796817227146",
+  appId: "1:796817227146:web:d1b69950b6d191ffd61c73"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
