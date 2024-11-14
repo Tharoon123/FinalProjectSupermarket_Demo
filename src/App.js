7ea1a1891af9ab7cf39cc47332c509ec36770b7f
Tharoon123 <64660264+Tharoon123@users.noreply.github.com>
2024-11-15 00:01:29 +0530

    Checkout screen created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './Components/HomePage';
+import CheckOut from './Components/CheckOut';
 
 function App() {
 
     //asdfadsfadsf
   return (
     <div>
-      <h1>
-        This is the Heading
-      </h1>
+  
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePage/>}></Route>
+          <Route path='/CheckOut' element={<CheckOut/>}></Route>
         </Routes>
       </BrowserRouter>
 
---
+++ src/Components/CheckOut.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CheckOut() {
+  return (
+    <div>
+        <h2>
+            THis is Checkout Page
+        </h2>
+    </div>
+  )
+}
+
+export default CheckOut
